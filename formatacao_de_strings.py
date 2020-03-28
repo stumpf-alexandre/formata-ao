d713c85a06ed56624116c536e912666b2ee4e4c4
Alexandre Stumpf <als.stumpf@gmail.com>
2020-03-28 00:43:29 -0300

    commit de formatação de strings

---
+++ formatacao_de_strings.py
@@ -0,0 +1,21 @@
+num1 = 3
+num2 = 10
+print("Tentativa {} de {}".format(num1, num2))
+print("Tentativa {1} de {0}".format(num1, num2))
+print("R$ {}".format(1.59))
+print("R$ {:f}".format(1.59))
+print("R$ {:2f}".format(1.59))
+print("R$ {:.2f}".format(1.5))
+print("R$ {:.2f}".format(1234.5))
+print("R$ {:7.2f}".format(1234.5))
+print("R$ {:7.2f}".format(4.5))
+print("R$ {:07.2f}".format(4.5))
+print("R$ {:08.3f}".format(4.5))
+print("R$ {:07d}".format(4))
+print("R$ {:07d}".format(46))
+print("R$ {:7d}".format(46))
+print(" {:07d}".format(4))
+print("Data {:2d}/{:2d}".format(9, 4))
+print("Data {:02d}/{:02d}".format(9, 4))
+print("Data {:02d}/{:02d}".format(19, 11))
+#https://docs.python.org/3/library/string.html#formatexamples
